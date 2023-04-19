5aeabf4c2050934dd42b12b4d5f7cd26a8819066
moonburnt <moonburnt@disroot.org>
2023-04-19 22:24:01 +0300

    add optional imgui support (enabled with WITH_IMGUI=ON compile flag)

---
+++ engine/imgui.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+// Everything imgui-related goes there
+// This gets included only if engine is compiled with WITH_IMGUI=ON option
+
+#include "imgui.h"
+#include "misc/cpp/imgui_stdlib.h"
+#include "rlImGui.h"
+#include "raylib.h"
+
+// For now, it does not define any logic
+// TODO - add imgui-specific nodes, scenes, components
